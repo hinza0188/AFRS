b23d10ad898a7d0791932f44bfa8b06c8fe2dbcd
Yongki JayZ An <hinza0188@gmail.com>
2016-10-04 22:28:54 -0400

    good

--- src/AFRS.java
+++ src/AFRS.java
@@ -1,8 +1,9 @@
 /**
- * Created by Jay An
+ * Created by Yongki Jay An
  */
 public class AFRS {
     public static void main(String[] args) {
+        // This is placeholder for github repository
         System.out.println("hi");
     }
 }
