3686e46e45de33bc80dd2aaef8e56ac8675df86b
Stevie Hetelekides <hetelek@gmail.com>
2016-10-06 17:01:58 -0400

    Print usage on startup

--- src/AFRS.java
+++ src/AFRS.java
@@ -6,7 +6,20 @@ public class AFRS
 {
     public static void main(String[] args)
     {
-        // This is placeholder for github repository
-        System.out.println("hi");
+        System.out.println("Welcome to the AFRS system.");
+        AFRS.printUsage();
+    }
+
+    private static void printUsage()
+    {
+        System.out.println("Usage:");
+        System.out.println("\tcommand_name required_arguments [option_arguments]");
+        System.out.println();
+
+        System.out.println("\tget_weather airport");
+        System.out.println("\tget_itinerary origin_airport destination_airport");
+        System.out.println("\tmake_reservation first_name last_name itinerary_id");
+        System.out.println("\tdelete_reservation first_name last_name origin_airport destination_airport");
+        System.out.println("\tget_reservation first_name last_name [origin_airport] [destination_airport]");
     }
 }
