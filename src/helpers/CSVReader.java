d47a37634151a15bcbcbb2bbeb51e1788b89082d
Stevie Hetelekides <hetelek@gmail.com>
2016-10-06 18:57:34 -0400

    Add reading CSV file class

---
+++ src/helpers/CSVReader.java
@@ -0,0 +1,53 @@
+/**
+ * Created by hetelek on 10/6/16.
+ */
+
+package helpers;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader
+{
+    private String filePath;
+    private BufferedReader br;
+
+    public CSVReader(String filePath)
+    {
+        this.filePath = filePath;
+    }
+
+    public void open() throws FileNotFoundException
+    {
+        this.br = new BufferedReader(new FileReader(this.filePath));
+    }
+
+    public void close()
+    {
+        try
+        {
+            this.br.close();
+        }
+        catch (IOException ex) { }
+    }
+
+    public String[] readLine()
+    {
+        try
+        {
+            String line = this.br.readLine();
+            if (line != null)
+            {
+                // use comma as separator
+                return line.split(",");
+            }
+        }
+        catch (Exception ex)  { }
+
+        // close and return
+        this.close();
+        return null;
+    }
+}
