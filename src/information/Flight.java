38a2d6e61fa32ed5f0b0610873805e1e9aa3205d
dxf7606 <dxf7606@cs.rit.edu>
2016-10-10 18:22:52 -0400

    Return departure time

--- src/information/Flight.java
+++ src/information/Flight.java
@@ -29,7 +29,7 @@ public class Flight implements Flyable
     @Override
     public LocalTime getDepartureTime()
     {
-        return null;
+        return this.departureTime;
     }
 
     @Override
