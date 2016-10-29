ecdf6fcb41df4aa2612df190d3cc96eb93109f02
Yongki Jay An <yxa8247@g.rit.edu>
2016-10-28 21:57:29 -0400

    put everything in start

--- src/gui/Client.java
+++ src/gui/Client.java
@@ -3,6 +3,7 @@ package gui;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -23,7 +24,6 @@ import server.UserSelector;
 
 public class Client extends Application {
     private UserSelector userSelector = new UserSelector();
-    private Group root = new Group();
 
     public static void main(String[] args) {
         launch(args);
@@ -36,101 +36,166 @@ public class Client extends Application {
         return tab;
     }
 
-    private HBox outputHBoxCenter() {
-        HBox obx = new HBox();
-        obx.setPadding(new Insets(15, 12, 15, 12));
-        obx.setSpacing(10);
+//    private HBox outputHBoxCenter() {
+//        HBox obx = new HBox();
+//        obx.setPadding(new Insets(15, 12, 15, 12));
+//        obx.setSpacing(10);
+//
+//        TextArea textArea = new TextArea();
+//        textArea.setId("outputField");
+//        textArea.setPrefSize(460, 380);
+//        textArea.setEditable(false);      // read-only
+//        textArea.setMouseTransparent(true);
+//        textArea.setFocusTraversable(false);
+//        obx.getChildren().add(textArea);
+//
+//        return obx;
+//    }
 
-        TextArea textArea = new TextArea();
-        textArea.setId("outputField");
-        textArea.setPrefSize(460, 380);
-        textArea.setEditable(false);      // read-only
-        textArea.setMouseTransparent(true);
-        textArea.setFocusTraversable(false);
-        obx.getChildren().add(textArea);
+//    private HBox inputHBoxBottom() {
+//        HBox ibx = new HBox();
+//        ibx.setPadding(new Insets(15, 12, 15, 12));
+//        ibx.setSpacing(10);
+//        //hb.setStyle("-fx-background-color: #336699;");
+//
+//        /* input field */
+//        TextField txtFld = new TextField ();
+//        txtFld.setId("inputField");
+//        txtFld.setPrefSize(400, 20);
+//        txtFld.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent event){
+//                // get the command string
+//                String txt = txtFld.getText();
+//
+//                String[] response = userSelector.takeCommand(txt);
+//                // TODO: write response to text area
+//                System.out.println(txt);
+//                // clear command area
+//                txtFld.clear();
+//            }
+//        });
+//
+//        /* input button */
+//        Button enter_btn = new Button();
+//        enter_btn.setId("EnterButton");
+//        enter_btn.setText("Enter");
+//        enter_btn.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent event) {
+//                // get the command string
+//                String txt = txtFld.getText();
+//
+//                String[] response = userSelector.takeCommand(txt);
+//                // TODO: write response to text area
+//
+//                // clear command area
+//                txtFld.clear();
+//            }
+//        });
+//        enter_btn.setPrefSize(100, 20);
+//
+//        ibx.getChildren().addAll(txtFld, enter_btn);
+//
+//        return ibx;
+//    }
 
-        return obx;
-    }
-
-    private HBox inputHBoxBottom() {
-        HBox ibx = new HBox();
-        ibx.setPadding(new Insets(15, 12, 15, 12));
-        ibx.setSpacing(10);
-        //hb.setStyle("-fx-background-color: #336699;");
-
-        /* input field */
-        TextField txtFld = new TextField ();
-        txtFld.setId("inputField");
-        txtFld.setPrefSize(400, 20);
-        txtFld.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event){
-                // get the command string
-                String txt = txtFld.getText();
-
-                String[] response = userSelector.takeCommand(txt);
-                // TODO: write response to text area
-                System.out.println(txt);
-                // clear command area
-                txtFld.clear();
-            }
-        });
-
-        /* input button */
-        Button enter_btn = new Button();
-        enter_btn.setId("EnterButton");
-        enter_btn.setText("Enter");
-        enter_btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                // get the command string
-                String txt = txtFld.getText();
-
-                String[] response = userSelector.takeCommand(txt);
-                // TODO: write response to text area
-
-                // clear command area
-                txtFld.clear();
-            }
-        });
-        enter_btn.setPrefSize(100, 20);
-
-        ibx.getChildren().addAll(txtFld, enter_btn);
-
-        return ibx;
-    }
-
-    private BorderPane borderContents() {
-        // create border pane
-        BorderPane page = new BorderPane();
-        page.setPadding(new Insets(0, 10, 0, 10));
-
-        // call input HBox at the bottom of the border pane
-        page.setBottom(inputHBoxBottom());
-
-        // call output HBox at the center of the border pane
-        page.setCenter(outputHBoxCenter());
-        return page;
-    }
+//    private BorderPane borderContents() {
+//        BorderPane page = new BorderPane();
+//        page.setPadding(new Insets(0, 10, 0, 10));
+//
+//        // call input HBox at the bottom of the border pane
+//        page.setBottom(inputHBoxBottom());
+//        // call output HBox at the center of the border pane
+//        page.setCenter(outputHBoxCenter());
+//        return page;
+//    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         // set current user
-        Scene scene = new Scene(this.root, 500, 450);
-
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 450);
         TabPane tabPane = new TabPane();
         BorderPane edge = new BorderPane();
 
+        // temporary hard coded tabs with
         for (int i = 1; i < 6; i++) {
-            Tab tab = new Tab();
-            tab.setText("Client_" + i);
+            Tab tab = new Tab(); // instanciates a new tab
+            tab.setText("Client_" + i); // name the tab
 
-            tab.setContent(borderContents());
-            tabPane.getTabs().add(tab);
+            /* create output box */
+            HBox obx = new HBox();
+            obx.setPadding(new Insets(15, 12, 15, 12));
+            obx.setSpacing(10);
+            TextArea textArea = new TextArea();
+            textArea.setId("outputField_" + i);
+            textArea.setPrefSize(460, 380);
+            textArea.setEditable(false);      // read-only
+            textArea.setMouseTransparent(true);
+            textArea.setFocusTraversable(false);
+            obx.getChildren().add(textArea);
+
+            /* create input box */
+            HBox ibx = new HBox();
+            ibx.setPadding(new Insets(15, 12, 15, 12));
+            ibx.setSpacing(10);
+                // input text field
+            TextField txtFld = new TextField ();
+            txtFld.setId("inputField_" + i);
+            txtFld.setPrefSize(400, 20);
+            txtFld.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    // get the command string
+                    String txt = txtFld.getText();
+
+                    String[] response = userSelector.takeCommand(txt);
+                    // TODO: write response to text area
+                    TextArea a = (TextArea)scene.lookup("#outputField_1");
+                    System.out.println(txt);
+                    a.setText(txt);
+
+                    // clear command area
+                    txtFld.clear();
+
+                }
+            }); // add event handler
+                // enter button
+            Button enter_btn = new Button();
+            enter_btn.setId("EnterButton_" + i);
+            enter_btn.setText("Enter");
+            enter_btn.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    // get the command string
+                    String txt = txtFld.getText();
+
+                    String[] response = userSelector.takeCommand(txt);
+                    // TODO: write response to text area
+
+                    // clear command area
+                    txtFld.clear();
+                }
+            });
+            enter_btn.setPrefSize(100, 20);
+            ibx.getChildren().addAll(txtFld, enter_btn);
+
+            /* cluster altogether into a border pane*/
+            BorderPane page = new BorderPane();
+            page.setPadding(new Insets(0, 10, 0, 10));
+
+                // call input HBox at the bottom of the border pane
+            page.setBottom(ibx);
+                // call output HBox at the center of the border pane
+            page.setCenter(obx);
+
+            /* add the page into the tab */
+            tab.setContent(page); // call contents constructor method
+            tabPane.getTabs().add(tab); // attach the tab to the tabPane
         }
         edge.prefHeightProperty().bind(scene.heightProperty());
         edge.prefWidthProperty().bind(scene.widthProperty());
-
         edge.setCenter(tabPane);
         root.getChildren().add(edge);
 
