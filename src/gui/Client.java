d0f824ac3ce41e8dec310ef06562251e15ced092
Yongki JayZ An <hinza0188@gmail.com>
2016-11-03 16:38:13 -0400

    chagned control

--- src/gui/Client.java
+++ src/gui/Client.java
@@ -4,12 +4,17 @@ import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -46,7 +51,7 @@ public class Client extends Application
      * @param txtFld
      * @param textArea
      */
-    private void inputAction(UserSelector user, Tab tab, TextField txtFld,  TextArea textArea)
+    private void inputAction(UserSelector user, Tab tab, TextField txtFld, TextArea textArea)
     {
         // select the appropriate user
         user.changeUser(tab.getText());
